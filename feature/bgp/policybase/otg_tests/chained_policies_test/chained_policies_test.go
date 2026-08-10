2c86578ba32d1b5d8bcd5809ac338117fcdd1618
Liam Walsh <liawalsh@cisco.com>
2026-08-10 14:37:34 -0400

    add exact BGP readiness checks to RT-1.29
    Ported-from: c85b4f494
    Traceability-ID: TEST-BUG-RT-1.29-READINESS

--- feature/bgp/policybase/otg_tests/chained_policies_test/chained_policies_test.go
+++ feature/bgp/policybase/otg_tests/chained_policies_test/chained_policies_test.go
@@ -229,11 +229,13 @@ func TestBGPChainedPolicies(t *testing.T) {
 				createFlow(t, td, tc.flowConfig)
 				td.verifyDUTBGPEstablished(t)
 				td.verifyOTGBGPEstablished(t)
+				td.awaitDestinationPrefix(t, tc.flowConfig, true)
 				checkTraffic(t, td, v4Flow)
 			} else {
 				createFlowV6(t, td, tc.flowConfig)
 				td.verifyDUTBGPEstablished(t)
 				td.verifyOTGBGPEstablished(t)
+				td.awaitDestinationPrefix(t, tc.flowConfig, false)
 				checkTraffic(t, td, v6Flow)
 			}
 		})
@@ -987,34 +989,36 @@ func (td *testData) advertiseRoutesWithEBGP(t *testing.T) {
 }
 
 func (td *testData) verifyDUTBGPEstablished(t *testing.T) {
-	sp := gnmi.OC().NetworkInstance(deviations.DefaultNetworkInstance(td.dut)).Protocol(oc.PolicyTypes_INSTALL_PROTOCOL_TYPE_BGP, bgpName).Bgp().NeighborAny().SessionState().State()
-	watch := gnmi.WatchAll(t, td.dut, sp, 2*time.Minute, func(val *ygnmi.Value[oc.E_Bgp_Neighbor_SessionState]) bool {
-		state, ok := val.Val()
-		if !ok || state != oc.Bgp_Neighbor_SessionState_ESTABLISHED {
-			return false
-		}
-		return true
-	})
-	if val, ok := watch.Await(t); !ok {
-		t.Fatalf("BGP sessions not established: got %v", val)
+	bgpPath := gnmi.OC().NetworkInstance(deviations.DefaultNetworkInstance(td.dut)).Protocol(oc.PolicyTypes_INSTALL_PROTOCOL_TYPE_BGP, bgpName).Bgp()
+	for _, neighbor := range []string{atePort1.IPv4, atePort2.IPv4, atePort1.IPv6, atePort2.IPv6} {
+		gnmi.Await(t, td.dut, bgpPath.Neighbor(neighbor).SessionState().State(), 2*time.Minute, oc.Bgp_Neighbor_SessionState_ESTABLISHED)
 	}
 	t.Log("DUT BGP sessions established")
 }
 
 // VerifyOTGBGPEstablished verifies on OTG BGP peer establishment
 func (td *testData) verifyOTGBGPEstablished(t *testing.T) {
-	sp := gnmi.OTG().BgpPeerAny().SessionState().State()
-	watch := gnmi.WatchAll(t, td.ate.OTG(), sp, 2*time.Minute, func(val *ygnmi.Value[otgtelemetry.E_BgpPeer_SessionState]) bool {
-		state, ok := val.Val()
-		if !ok || state != otgtelemetry.BgpPeer_SessionState_ESTABLISHED {
-			return false
-		}
-		return true
-	})
-	if val, ok := watch.Await(t); !ok {
-		t.Fatalf("BGP sessions not established: got %v", val)
+	for _, peer := range []string{
+		atePort1.Name + ".BGP4.peer",
+		atePort1.Name + ".BGP6.peer",
+		atePort2.Name + ".BGP4.peer",
+		atePort2.Name + ".BGP6.peer",
+	} {
+		gnmi.Await(t, td.ate.OTG(), gnmi.OTG().BgpPeer(peer).SessionState().State(), 2*time.Minute, otgtelemetry.BgpPeer_SessionState_ESTABLISHED)
 	}
 	t.Log("OTG BGP sessions established")
+}
+
+func (td *testData) awaitDestinationPrefix(t *testing.T, fc flowConfig, ipv4 bool) {
+	peerName := fc.src.Name + ".BGP6.peer"
+	if ipv4 {
+		peerName = fc.src.Name + ".BGP4.peer"
+		prefixPath := gnmi.OTG().BgpPeer(peerName).UnicastIpv4Prefix(fc.dstIP, v4RoutePrefix, otgtelemetry.UnicastIpv4Prefix_Origin_IGP, 0)
+		gnmi.Await(t, td.ate.OTG(), prefixPath.Address().State(), 2*time.Minute, fc.dstIP)
+		return
+	}
+	prefixPath := gnmi.OTG().BgpPeer(peerName).UnicastIpv6Prefix(fc.dstIP, v6RoutePrefix, otgtelemetry.UnicastIpv6Prefix_Origin_IGP, 0)
+	gnmi.Await(t, td.ate.OTG(), prefixPath.Address().State(), 2*time.Minute, fc.dstIP)
 }
 
 func configureDUT(t *testing.T, dut *ondatra.DUTDevice) {
