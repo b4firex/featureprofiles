af19fa579ce1556095c1f0565798413bec32f402
suredhar <suredhar@cisco.com>
2026-08-17 17:03:36 +0530

    Update setup.go

--- internal/gnmi/setup/setup.go
+++ internal/gnmi/setup/setup.go
@@ -229,7 +229,9 @@ func BuildBenchmarkingConfig(t *testing.T) *oc.Root {
 			intfName = dp.Name() + ".0"
 		}
 		isisIntf := isis.GetOrCreateInterface(intfName)
-		isisIntf.Enabled = ygot.Bool(true)
+		if deviations.ISISLevelEnabled(dut) {
+			isisIntfLevel.Enabled = ygot.Bool(true)
+		}
 		isisIntf.HelloPadding = oc.Isis_HelloPaddingType_ADAPTIVE
 		isisIntf.CircuitType = oc.Isis_CircuitType_POINT_TO_POINT
 		isisIntfAfi := isisIntf.GetOrCreateAf(oc.IsisTypes_AFI_TYPE_IPV4, oc.IsisTypes_SAFI_TYPE_UNICAST)
