3644021e871d77c73b26def011d40494fdc5c65b
Liam Walsh <liawalsh@cisco.com>
2026-08-21 14:59:20 -0400

    restore existing default-policy handling in RT-7.11
    Limit the parent-replace deviation to selecting the apply-policy write operation. Restore the preexisting default-policy construction without adding unrelated cleanup to this PR.
    
    Traceability-ID: REST-23627-PARENT-SCOPE

--- feature/bgp/policybase/otg_tests/import_export_multi_test/import_export_multi_test.go
+++ feature/bgp/policybase/otg_tests/import_export_multi_test/import_export_multi_test.go
@@ -552,10 +552,7 @@ func configureImportExportMultifacetMatchActionsBGPPolicy(t *testing.T, dut *ond
 	pathV61 := gnmi.OC().NetworkInstance(dni).Protocol(oc.PolicyTypes_INSTALL_PROTOCOL_TYPE_BGP, bgpName).Bgp().Neighbor(ipv61).AfiSafi(oc.BgpTypes_AFI_SAFI_TYPE_IPV6_UNICAST).ApplyPolicy()
 	policyV61 := root.GetOrCreateNetworkInstance(dni).GetOrCreateProtocol(oc.PolicyTypes_INSTALL_PROTOCOL_TYPE_BGP, bgpName).GetOrCreateBgp().GetOrCreateNeighbor(ipv61).GetOrCreateAfiSafi(oc.BgpTypes_AFI_SAFI_TYPE_IPV6_UNICAST).GetOrCreateApplyPolicy()
 	policyV61.SetExportPolicy([]string{parentPolicy})
-	if deviations.BgpPolicyLeafListsRequireParentReplace(dut) && !deviations.DefaultRoutePolicyUnsupported(dut) {
-		policyV61.SetDefaultImportPolicy(oc.RoutingPolicy_DefaultPolicyType_REJECT_ROUTE)
-		policyV61.SetDefaultExportPolicy(oc.RoutingPolicy_DefaultPolicyType_REJECT_ROUTE)
-	} else if !deviations.DefaultRoutePolicyUnsupported(dut) {
+	if !deviations.DefaultRoutePolicyUnsupported(dut) {
 		policyV6.SetDefaultImportPolicy(oc.RoutingPolicy_DefaultPolicyType_REJECT_ROUTE)
 		policyV6.SetDefaultExportPolicy(oc.RoutingPolicy_DefaultPolicyType_REJECT_ROUTE)
 	}
@@ -571,10 +568,7 @@ func configureImportExportMultifacetMatchActionsBGPPolicy(t *testing.T, dut *ond
 	pathV41 := gnmi.OC().NetworkInstance(dni).Protocol(oc.PolicyTypes_INSTALL_PROTOCOL_TYPE_BGP, bgpName).Bgp().Neighbor(ipv41).AfiSafi(oc.BgpTypes_AFI_SAFI_TYPE_IPV4_UNICAST).ApplyPolicy()
 	policyV41 := root.GetOrCreateNetworkInstance(dni).GetOrCreateProtocol(oc.PolicyTypes_INSTALL_PROTOCOL_TYPE_BGP, bgpName).GetOrCreateBgp().GetOrCreateNeighbor(ipv41).GetOrCreateAfiSafi(oc.BgpTypes_AFI_SAFI_TYPE_IPV4_UNICAST).GetOrCreateApplyPolicy()
 	policyV41.SetExportPolicy([]string{parentPolicy})
-	if deviations.BgpPolicyLeafListsRequireParentReplace(dut) && !deviations.DefaultRoutePolicyUnsupported(dut) {
-		policyV41.SetDefaultImportPolicy(oc.RoutingPolicy_DefaultPolicyType_REJECT_ROUTE)
-		policyV41.SetDefaultExportPolicy(oc.RoutingPolicy_DefaultPolicyType_REJECT_ROUTE)
-	} else if !deviations.DefaultRoutePolicyUnsupported(dut) {
+	if !deviations.DefaultRoutePolicyUnsupported(dut) {
 		policyV4.SetDefaultImportPolicy(oc.RoutingPolicy_DefaultPolicyType_REJECT_ROUTE)
 		policyV4.SetDefaultExportPolicy(oc.RoutingPolicy_DefaultPolicyType_REJECT_ROUTE)
 	}
